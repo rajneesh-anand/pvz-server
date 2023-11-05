ccdcd968d36500e6e40d7cdf1bf4402e42d9567a
anand.k.rajneesh@gmail.com <anand.k.rajneesh@gmail.com>
2023-11-05 16:26:13 +0530

    item

--- routes/item.js
+++ routes/item.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const prisma = require("../lib/prisma");
+const path = require("path");
+const fs = require("fs/promises");
+const { IncomingForm } = require("formidable");
+const DatauriParser = require("datauri/parser");
+const cloudinary = require("cloudinary").v2;
+const parser = new DatauriParser();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFileToCloudinary = async (file) => {
+  const docContent = await fs
+    .readFile(file.path)
+    .catch((err) => console.error("Failed to read file", err));
+  const doc64 = parser.format(path.extname(file.name).toString(), docContent);
+  const uploadResult = await cloudinary.uploader.upload(doc64.content);
+  return uploadResult;
+};
 
 const router = express.Router();
-
-// const APP_URL = "http://localhost:8800/api";
-const APP_URL = "https://yasha64.ru/api";
 
 function paginate(totalItems, currentPage, pageSize, count, url) {
   const totalPages = Math.ceil(totalItems / pageSize);
@@ -29,58 +47,78 @@ function paginate(totalItems, currentPage, pageSize, count, url) {
     firstItem: startIndex,
     lastItem: endIndex,
     perPage: pageSize,
-    first_page_url: `${APP_URL}${url}&page=1`,
-    last_page_url: `${APP_URL}${url}&page=${totalPages}`,
+    first_page_url: `${process.env.API_URL}${url}&page=1`,
+    last_page_url: `${process.env.API_URL}${url}&page=${totalPages}`,
     next_page_url:
       totalPages > currentPage
-        ? `${APP_URL}${url}&page=${Number(currentPage) + 1}`
+        ? `${process.env.API_URL}${url}&page=${Number(currentPage) + 1}`
         : null,
     prev_page_url:
-      totalPages > currentPage ? `${APP_URL}${url}&page=${currentPage}` : null,
+      totalPages > currentPage
+        ? `${process.env.API_URL}${url}&page=${currentPage}`
+        : null,
   };
 }
 
-router.get("/product-list", async (req, res) => {
-  const curPage = req.query.page || 1;
-  const perPage = req.query.limit || 25;
+// router.get("/list", async (req, res) => {
+//   const curPage = req.query.page || 1;
+//   const perPage = req.query.limit || 25;
 
-  const url = `/product-list?limit=${perPage}`;
+//   const url = `/item/list?limit=${perPage}`;
 
-  const skipItems =
-    curPage == 1 ? 0 : (parseInt(perPage) - 1) * parseInt(curPage);
+//   const skipItems =
+//     curPage == 1 ? 0 : (parseInt(perPage) - 1) * parseInt(curPage);
 
-  const totalItems = await prisma.product.count();
+//   const totalItems = await prisma.blog.count();
+//   try {
+//     const results = await prisma.blog.findMany({
+//       skip: skipItems,
+//       take: parseInt(perPage),
+//       select: {
+//         id: true,
+//         title: true,
+//         description: true,
+//         image: true,
+//         status: true,
+//       },
+//       orderBy: {
+//         createdAt: "desc",
+//       },
+//     });
+//     return res.status(200).json({
+//       msg: "success",
+//       blogs: results,
+//       ...paginate(totalItems, curPage, perPage, results.length, url),
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//     return res.status(400).json({ message: error.message });
+//   } finally {
+//     async () => {
+//       await prisma.$disconnect();
+//     };
+//   }
+// });
+
+router.get("/list", async (req, res) => {
+  let page = req.query.page;
+  let limit = 25;
 
   try {
-    const results = await prisma.product.findMany({
-      skip: skipItems,
-      take: parseInt(perPage),
-      where: {
-        status: `{"label":"Active","value":"Active"}`,
-      },
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        slug: true,
-        category: true,
-        image: true,
-        coinValue: true,
-        inStock: true,
-      },
+    const items = await prisma.item.findMany({
+      skip: page == 1 ? 0 : Number(page) * 50,
+      take: Number(limit),
       orderBy: {
-        createdAt: "desc",
+        id: "desc",
       },
     });
-    // console.log(results);
+
     return res.status(200).json({
-      msg: "success",
-      products: results,
-      ...paginate(totalItems, curPage, perPage, results.length, url),
+      data: items,
     });
   } catch (error) {
-    console.log(error.message);
-    return res.status(500).send({ message: error.message });
+    console.log(error);
+    return res.status(400).json({ message: error.message });
   } finally {
     async () => {
       await prisma.$disconnect();
@@ -88,28 +126,177 @@ router.get("/product-list", async (req, res) => {
   }
 });
 
-router.get("/product/:slug", async (req, res) => {
-  const slug = req.params.slug;
-
+router.get("/items-list", async (req, res) => {
   try {
-    const product = await prisma.product.findFirst({
+    const items = await prisma.item.findMany({
       where: {
-        slug: slug,
+        status: "Active",
       },
       select: {
         id: true,
         name: true,
-        description: true,
-        category: true,
+        price: true,
+        salePrice: true,
+        discount: true,
         image: true,
-        coinValue: true,
+        marketPlace: true,
+        inStock: true,
+        link: true,
+        createdAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return res.status(200).json({ items });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error.message });
+  } finally {
+    async () => {
+      await prisma.$disconnect();
+    };
+  }
+});
+
+router.post("/create", async (req, res) => {
+  const data = await new Promise((resolve, reject) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) return reject(err);
+      resolve({ fields, files });
+    });
+  });
+
+  // console.log(data);
+
+  const imageUrl =
+    Object.keys(data.files).length > 0
+      ? await uploadFileToCloudinary(data.files.image)
+      : null;
+
+  const discount =
+    ((Number(data.fields.price) - Number(data.fields.salePrice)) /
+      Number(data.fields.price)) *
+    100;
+  console.log(discount);
+
+  try {
+    await prisma.item.create({
+      data: {
+        name: data.fields.name,
+        link: data.fields.link,
+        description: data.fields.description,
+        image: imageUrl
+          ? imageUrl.secure_url
+          : "https://res.cloudinary.com/dlywo5mxn/image/upload/v1693041477/no-image_b3mfoq.png",
+
+        price: Number(data.fields.price),
+        salePrice: Number(data.fields.salePrice),
+        discount: discount,
+        inStock: Number(data.fields.inStock),
+        category: data.fields.category,
+        status: data.fields.status,
+        marketPlace: data.fields.marketPlace,
       },
     });
 
-    console.log(product);
-    return res.status(200).json({
-      product: product,
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).json({ message: error.message });
+  } finally {
+    async () => {
+      await prisma.$disconnect();
+    };
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await new Promise((resolve, reject) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) return reject(err);
+      resolve({ fields, files });
     });
+  });
+
+  // console.log(data);
+
+  const imageUrl =
+    Object.keys(data.files).length > 0
+      ? await uploadFileToCloudinary(data.files.image)
+      : null;
+
+  const discount =
+    ((Number(data.fields.price) - Number(data.fields.salePrice)) /
+      Number(data.fields.price)) *
+    100;
+  console.log(discount);
+
+  try {
+    if (Object.keys(data.files).length > 0) {
+      await prisma.item.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          name: data.fields.name,
+          link: data.fields.link,
+          description: data.fields.description,
+          image: imageUrl.secure_url,
+          price: Number(data.fields.price),
+          salePrice: Number(data.fields.salePrice),
+          discount: discount,
+          inStock: Number(data.fields.inStock),
+          category: data.fields.category,
+          status: data.fields.status,
+          marketPlace: data.fields.marketPlace,
+        },
+      });
+    } else {
+      await prisma.item.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          name: data.fields.name,
+          link: data.fields.link,
+          description: data.fields.description,
+          price: Number(data.fields.price),
+          salePrice: Number(data.fields.salePrice),
+          discount: discount,
+          inStock: Number(data.fields.inStock),
+          category: data.fields.category,
+          status: data.fields.status,
+          marketPlace: data.fields.marketPlace,
+        },
+      });
+    }
+
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).json({ message: error.message });
+  } finally {
+    async () => {
+      await prisma.$disconnect();
+    };
+  }
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const result = await prisma.item.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    return res.status(200).json({ message: "success", data: result });
   } catch (error) {
     console.log(error.message);
     return res.status(400).json({ message: error.message });
